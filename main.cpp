bbdb407810028c99b35874cbdf623e3f7644c6bd
孙达明 <83948628+tyza66@users.noreply.github.com>
2026-03-26 08:51:50 +0800

    feat: 继续适配mac版本

--- main.cpp
+++ main.cpp
@@ -40,7 +40,9 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     
     // 设置应用程序图标
+#ifndef Q_OS_MACOS
     app.setWindowIcon(QIcon(":/qt/qml/Honeycomb/logo.png"));
+#endif
 
     // 设置 Qt Quick Controls 样式为 Basic，支持自定义控件外观
     QQuickStyle::setStyle("Basic");
